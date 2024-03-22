aa38dee8c4167375ddb7fd2f9c247469b952af26
unknown <tejasraibs@gmail.com>
2024-03-22 10:48:20 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      'mongodb+srv://d06eeksha:Deeksha%401106@cluster0.ws2ybwa.mongodb.net/Profile?retryWrites=true&w=majority&appName=Cluster0'
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
